7a28b6f364af3ae7c7b6db347f264a44b72af894
scapula07 <bartholomewonogwu@yahoo.com>
2023-06-15 10:39:54 +0100

    new commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+
+
+# Base image with Node.js
+#FROM node:latest
+FROM node:16-alpine
+
+# Install FFmpeg
+RUN apt-get update && apt-get install -y ffmpeg
+
+# Set the working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json (if present)
+COPY package*.json ./
+
+# Install Node.js dependencies
+RUN npm install
+
+# Copy the application code
+COPY . .
+
+# Expose any necessary ports (if applicable)
+EXPOSE 3000
+
+# Start the application
+CMD [ "npm", "start" ]
+
+# FROM node:16-alpine
+# WORKDIR /app
+# ADD . .
+# RUN npm install
+# EXPOSE 3000
+# CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const child_process = require('child_process'); // To be used later for running FFmpeg
-const express = require('express');
+const child_process = require('child_process');
 const http = require('http');
 const WebSocketServer = require('ws').Server;
+const fs = require("fs");
 
 
-
+const PORT = process.env.PORT || 5001;
 const wss = new WebSocketServer({
-  port:5001
+  port:PORT
 });
 
 
@@ -17,25 +17,63 @@ wss.on('connection', (ws, req) => {
     // '-f', 'lavfi', '-i', 'anullsrc',
     
 
-    '-i', '-',
+    // '-i', '-',
+   '-i', '-', '-v', 'error',
+
     
     // '-shortest',
     
 
-    '-vcodec', 'copy',
+    // '-vcodec', 'copy',
     
    
-    '-acodec', 'aac',
+    // '-acodec', 'aac',
  
-    '-f', 'flv',
+    // '-f', 'flv',
     
-    
-    'vid.flv',
- 
+    // 'rtmp://rtmp.livepeer.com/live/5421-cpzy-1w4f-grwt',
+    // "rtmp://a.rtmp.youtube.com/live2/admv-yg7c-gd0s-566q-07s3"
+    // 'browser.flv',
+
+  //   '-vcodec','libx264', '-pix_fmt','yuv420p', '-preset' ,'medium', '-r', '30 ' ,'-g', '60', '-b:v', '2500k',
+  //   '-acodec', 'libmp3lame', '-ar','44100', '-threads' ,'6','-qscale' ,'3' ,'-b:a' ,'712000','-bufsize', '512k',
+  // '-f','flv', 'rtmp://rtmp.livepeer.com/live/5421-cpzy-1w4f-grwt'
+
+  // '-c:v',
+  // 'libx264',
+  // '-preset',
+  // 'veryfast',
+  // '-tune',
+  // 'zerolatency',
+  // '-g:v',
+  // '60',
+
+  // // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+  // '-c:a',
+  // 'aac',
+  // '-strict',
+  // '-2',
+  // '-ar',
+  // '44100',
+  // '-b:a',
+  // '64k',
+
+  // //force to overwrite
+  // '-y',
+
+  // // used for audio sync
+  // '-use_wallclock_as_timestamps',
+  // '1',
+  // '-async',
+  // '1',
+
+  // '-f',
+  // 'flv',
+
+'-c:v', 'libx264', '-x264-params', 'keyint=60:no-scenecut=1' ,'-c:a', 'copy', '-f', 'flv',
+'rtmp://rtmp.livepeer.com/live/d40f-3nc3-sl74-ja7t'
+
    ])
-
-
-
 
    ffmpeg.on('close', (code, signal) => {
     console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
@@ -57,18 +95,25 @@ wss.on('connection', (ws, req) => {
 
  
   ws.on('message', (msg) => {
-    console.log('DATA', msg);
+    console.log('DATA Transcoding', msg);
     ffmpeg.stdin.write(msg);
   });
   
   wss.on('close', (e) => {
     ffmpeg.kill('SIGINT');
   });
+
+  ffmpeg.stdout.on('data', function(data) {
+    console.log( data.toString(),"data out");
+  
+   
+  });
   
 });
 
 
 
-  
 
+
+  
 
